165f068794def5177d1e2731e0b01d9696499a5b
FlRE-DEATH <mrobert.bencze@stud.etti.upb.ro>
2024-03-05 21:34:36 +0200

    Cleaned some code

--- test-mix-console.py
+++ test-mix-console.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any, List
 from utils import *
 
 
@@ -5,6 +6,7 @@ from utils import *
 # https://codeandsound.wordpress.com/2014/10/09/parametric-eq-in-python/
 # https://github.com/topics/equalizer?l=python
 
+# noinspection PyMethodMayBeStatic,PyAttributeOutsideInit
 class SignalProcessor:
 
     def __init__(self, rate=None):
@@ -18,6 +20,7 @@ class SignalProcessor:
         self.filters = []
 
     ###############################>> PROC VARS CREATION <<######################################################
+
     def _create_proc_vars_single_filter_type_name(self, dict_param_names_and_ranges):
         """
         This function generates all possible combinations for a single filter type and outputs them as a list.
@@ -43,7 +46,7 @@ class SignalProcessor:
         :param dict_all_filter_settings_ranges:
         :return:
         """
-        dict_unraveled_all_filter_settings = {}
+        dict_unraveled_all_filter_settings: dict[Any, list[dict[Any, Any]]] = {}
 
         for filter_type in dict_all_filter_settings_ranges:
             out_list = self._create_proc_vars_single_filter_type_name(dict_all_filter_settings_ranges[filter_type])
@@ -54,11 +57,14 @@ class SignalProcessor:
                                            start_index=0,
                                            end_index=None,
                                            cr_proc_number_of_filters=None):
+
         # TODO check if combinations contain all the 5 filters and no repeated filters
         """
         This function takes a dict of all possible filter settings and creates all possible combinations of them.
-        :param cr_proc_number_of_filters: If not None, this restricts the possible combinations to the subset containing exactly number_of_filters filters
-        :param dict_proc_vars_multiple_filter_type_names:  dict of all possible filter settings - ranges for the filter parameters along with the filter type names
+        :param cr_proc_number_of_filters: If not None, this restricts the possible combinations
+                                    to the subset containing exactly number_of_filters filters
+        :param dict_proc_vars_multiple_filter_type_names:  dict of all possible filter settings - ranges for
+                                    the filter parameters along with the filter type names
         :param root_filename: the name of the file that will be saved after processing
         :param start_index: the start index of the file and of the processing variant (can be used for parallelization)
         :param end_index: the end index of the file and of the processing variant (can be used for parallelization)
@@ -76,12 +82,14 @@ class SignalProcessor:
                     result.append(current_combination.copy())
                     return
 
-                for i in range(index, len(keys)):
-                    current_key = keys[i]
+                for j in range(index, len(keys)):
+                    current_key = keys[j]
                     for element in input_dict[current_key]:
-                        # if element not in current_combination.values(): #TODO this sometimes removes valid combinations - falsely rejects combinations that are NOT YET in the output
+                        # if element not in current_combination.values():
+                        # TODO this sometimes removes valid combinations - falsely rejects combinations
+                        #  that are NOT YET in the output
                         current_combination[current_key] = element
-                        recursive_backtrack(i + 1, current_combination)
+                        recursive_backtrack(j + 1, current_combination)
                         del current_combination[current_key]
 
             recursive_backtrack(0, {})
@@ -116,11 +124,11 @@ class SignalProcessor:
 
             dict_unraveled_filter_settings = self._create_proc_vars_multiple_filter_type_names(
                 dict_param_names_and_ranges)
-            dict_filenames_and_process_variants = self._create_all_proc_vars_combinations(
+            ete_dict_filenames_and_process_variants = self._create_all_proc_vars_combinations(
                 dict_unraveled_filter_settings,
                 root_filename, start_index,
                 end_index, number_of_filters)
-            return dict_filenames_and_process_variants
+            return ete_dict_filenames_and_process_variants
         else:  # TODO test this
             raise Exception(f"Number of filters in dict_param_names_and_ranges ({len(dict_param_names_and_ranges)}) "
                             f"does not match the number_of_filters ({number_of_filters}).")
@@ -128,8 +136,6 @@ class SignalProcessor:
     ############################################################################################################
     def _create_filter(self, filter_type_name, dict_params):
         """
-
-        :param filter:
         :param filter_type_name: one of: low_pass, high_pass, peak, low_shelf, high_shelf
         :param dict_params: IF [filter_type_name] in [low_pass, high_pass] -
                        dict like {
@@ -173,10 +179,10 @@ class SignalProcessor:
                                                                     **dict_procs_single_variant[crt_filter_type_name]})
         return self.filters
 
-    def _process_signal_variant(self, signal_in, dict_procs_single_variant):
+    def _process_signal_variant(self, pv_signal_in, dict_procs_single_variant):
         """
         Outputs the input signal with all the processings in the dict_procs_single_variant
-        :param signal_in:
+        :param pv_signal_in:
         :param dict_procs_single_variant:
         example:
          {
@@ -188,10 +194,10 @@ class SignalProcessor:
         """
 
         self._create_filters_single_proc(dict_procs_single_variant)
-        signal_out = signal_in.copy()
-        print("line 93 preproc comp in/out", signal_in == signal_out)  # test if the signal was copied
+        signal_out = pv_signal_in.copy()
+        print("line 93 preproc comp in/out", pv_signal_in == signal_out)  # test if the signal was copied
         for f in self.filters:
-            print("crt filter coeffs: a", f._a_coeffs, "b", f._b_coeffs)
+            # print("crt filter coeffs: a", f._a_coeffs, "b", f._b_coeffs)
             sig_out_pre = signal_out.copy()
             f.process(signal_out, signal_out)
             # time.sleep(0.01) # wait 10 ms
@@ -200,7 +206,7 @@ class SignalProcessor:
         self.reset()  # after signal variant was processed, reset
         return signal_out
 
-    def process_signal_all_variants(self, signal_in, dict_filenames_and_process_variants, normalize=True):
+    def process_signal_all_variants(self, asv_signal_in, asv_dict_filenames_and_process_variants, normalize=True):
 
         # this function that calls _process_signal_variant multiple times by iterating a dict of dicts.
         # A processing list is a dict of { filename: {filter_type_name: {param_name: value} } }
@@ -208,33 +214,34 @@ class SignalProcessor:
         # the name of the signal will be like base_name_index
 
         # TODO maybe modify to first call the create_all_proc_vars and then the backtracking idk
-        for filename in dict_filenames_and_process_variants:
+        for filename in asv_dict_filenames_and_process_variants:
             # out_sig = signal_in.copy() #added
             # for filter_type in dict_filenames_and_process_variants[filename]: # added
-            out_sig = self._process_signal_variant(signal_in, dict_filenames_and_process_variants[
+            out_sig = self._process_signal_variant(asv_signal_in, asv_dict_filenames_and_process_variants[
                 filename])  # signal_in -> out_sig
             if normalize:
                 out_sig = normalization(out_sig)
             sf.write(filename, out_sig, self.rate)
 
             reset = True
-            for filter_type in dict_filenames_and_process_variants[filename]:
+            for filter_type in asv_dict_filenames_and_process_variants[filename]:
                 # write signal to disk using metadata as well. write metadata one by one
-                write_metadata(filename, filter_type, str(dict_filenames_and_process_variants[filename][filter_type]),
+                write_metadata(filename, filter_type,
+                               str(asv_dict_filenames_and_process_variants[filename][filter_type]),
                                reset, False, True)
                 reset = False
 
 
 # contact@romainclement.com
-# signal_in, sr = sf.read('D:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav')
-signal_in, sr = sf.read('D:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise-mono.wav')
+# signal_in, sr = sf.read(r'D:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav')
+signal_in, sr = sf.read(r'D:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise-mono.wav')
 # signal_in, sr = sf.read('eq_ed_20.wav')
 # signal_in, sr = sf.read('c_eq_ed_10-20.wav')
 
 aas = SignalProcessor(sr)
 
-# Usage tips: You need to add numbers at the end of every signal processing type, because you can have multiple of the same type
-# like peak1, peak2, peak3 etc - always name them with numbers at the end
+# Usage tips: You need to add numbers at the end of every signal processing type, because
+# you can have multiple of the same type like peak1, peak2, peak3 etc - always name them with numbers at the end
 
 # Usage tips: include dbgain 0 if you want to ignore a certain type of filter OR remove it from the below dict.
 dict_all_filter_settings = {
@@ -246,7 +253,8 @@ dict_all_filter_settings = {
     "low_pass": {"cutoff": range(1000, 1001, 1000), "resonance": range(2, 3)},
     "high_shelf": {"cutoff": [1000], "resonance": [2], "dbgain": [2]}
 }
-# Change this to the number of filters you want to use or None to use all possible combinations of filters, any number of filters.
+# Change this to the number of filters you want to use or None
+# to use all possible combinations of filters, any number of filters.
 no_filters = len(dict_all_filter_settings)
 
 # aas._create_all_proc_vars_combinations()
@@ -275,7 +283,7 @@ print(aas.filters)
 #  4) output a dict of {filename_index (or index_comb): process_variant_dict}
 
 
-path = 'F:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav'
+path = r'F:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav'
 
 """
     This utility will create variations of a signal based on the input parameters and their values.
@@ -285,15 +293,18 @@ path = 'F:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav'
         // sample rate is extracted from the signal
         list of dict like - {param_name: list - [Param start, Param stop, Params increment, Params increment scale (log or linear)] }
         ^ this dict is the base, for a single processing. 
-        // TODO see how to make multiple processing, like combine 2 dicts. - solution: have multiple param names in a list. 
-        // first put simple dicts then put multiple param names in the same dict. if the dict contains multiple parameters, 
+        // TODO see how to make multiple processing, like combine 2 dicts. 
+        - solution: have multiple param names in a list. 
+        // first put simple dicts then put multiple param names in the same dict. 
+        if the dict contains multiple parameters, 
             then it means those params will be combined and there will be nested for loops. 
             the number of for loops depends on the number of dict keys. 
             there will be a mandatory "outer" for loop that iterates through all dicts.
             
             // to do all combinations of dicts for the "inner" for loops, backtracking will be required. 
         
-        {   Param name is based on the processing type (filter type or comp param) and the param name. for instance, see below
+        {   Param name is based on the processing type (filter type or comp param) and the param name. 
+        for instance, see below:
             list of dicts with processings
             [
                 + allowed params for low/high pass (seemingly constant db/octave):
@@ -332,9 +343,3 @@ path = 'F:\PCON\Disertatie\AutoMixMaster\datasets\diverse-test\white-noise.wav'
             // WHAT FORMAT WOULD IT BE BEST? - structured data with a fixed set of columns.  
 
 """
-
-# save out the processed audio
-# wavfile.write("output_yodel.wav",rate,out)
-# sf.write(f"output_loshelf{str(gain)}dB_Q10_1k.wav", out, rate) # TODO what to use for writing? librosa, wavfile?
-
-# TODO maybe pymixconsole gave wrong results due to the incorrect read/write of the audio
