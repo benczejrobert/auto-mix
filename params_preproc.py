865be803cf7e5f54c3a2cbc059f7920a5b35630a
FlRE-DEATH <mrobert.bencze@stud.etti.upb.ro>
2024-06-18 16:49:23 +0300

    Moved crt run index from main and added real param variations

--- params_preproc.py
+++ params_preproc.py
@@ -13,22 +13,72 @@ sig_root_path = "..\\data\\raw-audio\\"
 preproc_signals_root_folder = "..\\data\\processed-audio-latest"
 sample_rate = 22050
 # TODO when multiple channels will be added, this dict can be added to a list of dicts, one for each channel
+param_start_index = 0  # left at 65613 for commented generic params Moro
 
 # Usage tips: You need to add numbers at the end of every signal processing type, because
 # you can have multiple of the same type such as peak1, peak2, peak3 etc. - always name them with numbers at the end
 
 # Usage tips: include dbgain 0 if you want to ignore a certain type of filter OR remove it from the below dict.
+
 # TODO keep in mind that the order of files is important.
 #  If files/channel wavs are not numbered in the sig_root_path,
 #  the processing dicts might be applied in a different order.
-list_dict_all_filter_settings = [{
-    "high_pass": {"cutoff": range(200, 201, 1000), "resonance": range(2, 3)},
-    "low_shelf": {"cutoff": range(200, 201, 1000), "resonance": range(2, 3), "dbgain": list(range(12, 13, 11))},
-    "peak1": {"center": range(1000, 7001, 3000), "resonance": range(2, 3), "dbgain": list(range(-40, 41, 11))},
-    "peak2": {"center": range(8000, 8001), "resonance": range(2, 3), "dbgain": [40]},
-    "low_pass": {"cutoff": range(10000, 10001, 1000), "resonance": range(2, 3)},
-    "high_shelf": {"cutoff": [9000], "resonance": [2], "dbgain": [0]}
-}] * 2
+
+# TODO maybe try using different ranges
+#  for each channel rather than a generic set of params
+
+# gen_hp_freq = list(range(30, 100, 10))
+# gen_hp_freq.extend(range(150, 400, 50))
+#
+# gen_ls_freq = range(100, 301, 50)
+# gen_p1_freq = list(range(200, 1001, 100))  # does not cover the kick's peak
+# kick_q_interval = [1 / i for i in range(1, 4)]
+# gen_gain_interval = list(range(-12, 13, 3)) # checkif any gains lower than -12 in reaper (and if not too much data will be created)
+#
+# gen_p2_freq = list(range(1000, 5001,500)) # does not cover the kick's peak
+# # high shelf
+# gen_hs_freq = list(range(5000,10001,1000))
+#
+#
+# # low pass log-ish interval
+# gen_lp_freq = list(range(100,451,50))  # generates 1.6 million files until here
+# gen_lp_freq.extend(range(500, 1501, 100)) # generates 16 million files until here
+# gen_lp_freq.extend(range(1500, 5001, 500))  # 118 million files until here
+# gen_lp_freq.extend(range(5000, 10001, 1000))  # 1.18 billion files until here
+
+# gen_lp_freq.extend(range(5000, 15001, 1000))  # if no resampling is considered
+
+
+# TODO check intervals for the frequencies to match the snare and kick freqs
+
+# TODO also check how to make sure that the channels correpsond to their filter settings within the list.
+#  maybe do a dict and use keys as file names or some identifier found in the file name (like regex match) or skip step if no match
+
+general_ls_freq = range(150, 301, 60)
+general_q_interval = [0.5, 1]
+general_gain_interval = [-9, -5, 0, 5, 9]  # checkif any gains lower than -12 in reaper (and if not too much data will be created)
+
+kick_hp_freq = list(range(30, 101, 15))
+kick_hp_freq.extend(range(150, 251, 50))
+
+kick_p1_freq = list(range(50, 351, 50))  # does not cover the kick's peak
+
+kick_p2_freq = list(range(400, 801,100))
+
+
+
+
+
+list_dict_all_filter_settings = [
+{  # kick frequency settings
+    "high_pass": {"cutoff": kick_hp_freq, "resonance": [0.5]},
+    "low_shelf": {"cutoff": general_ls_freq, "resonance": general_q_interval, "dbgain": general_gain_interval},
+    "peak1": {"center": kick_p1_freq, "resonance": general_q_interval, "dbgain": general_gain_interval},
+    "peak2": {"center": kick_p2_freq, "resonance": general_q_interval, "dbgain": general_gain_interval},
+    "low_pass": {"cutoff": [20000], "resonance": [0.5]},  # maybe ignore for the purpose of the kick in and snr top
+    "high_shelf": {"cutoff": [20000], "resonance": [1], "dbgain": [0]}  # maybe ignore for the purpose of the kick in and snr top. TODO decrease the sr threshold
+},  # for kick in and snr top. sunt ignorate bine peste 10k, deci resample ok
+] * 2
 
 dict_normalization_values = { "dbgain_min": -40,
                               "dbgain_max": 40,
