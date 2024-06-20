f2c4b10b6dfc27a2afd3e168ebdcf940d8e5b198
FlRE-DEATH <mrobert.bencze@stud.etti.upb.ro>
2024-06-20 12:16:04 +0300

    Comments added, params modified

--- params_features.py
+++ params_features.py
@@ -3,9 +3,14 @@ from params_preproc import *
 
 # Feature extractor
 # todo here will be a subfolder structure for each drum channel
+# TODO create a vizualization for a smaller subset of features BEFORE the feature extraction just to see their separability etc.
+#  and maybe a PCA analysis as well
+#  + to see if the features are extracted correctly
+
+
 extracted_features_folder = "..\\data\\features-latest"
 
-feature_dict = {'sr': sample_rate, 'n_fft': 4096*4, 'n_mfcc': 26, 'hop_length': 512, 'margin': 3.0, 'n_lvls':5,'wavelet_type':'db1'}
+feature_dict = {'sr': sample_rate, 'n_fft': 4096, 'n_mfcc': 26, 'hop_length': 512, 'margin': 3.0, 'n_lvls':5,'wavelet_type':'db1'}
 # feature_list = ['mfcc'] # ['spect', 'mel_spect']
 feature_list = ['fft'] # ['spect', 'mel_spect']
 # feature_list = ['cepstrum'] # ['spect', 'mel_spect']
@@ -13,5 +18,5 @@ variance_type = 'smad'  #[string], type of variance, either 'var' or 'smad'
 raw_features = True #[bool] if True, skips mean and var extraction from the audio features in the feature list
 keep_feature_dims = True #[bool] if True, do not reduce individual features' dimensions to 1D shape. Only useful if raw_features is True
 
-param_pre_diff = True # this parameter specifies if the difference is made before or after the features are extracted.
+param_pre_diff = False # this parameter specifies if the difference is made before or after the features are extracted.
                     # if True, the features are extracted from the diff. if False, the diff is made from the features.
