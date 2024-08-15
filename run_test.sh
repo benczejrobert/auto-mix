0a369c66a981ec773f0a0df463e0c750cbb58cc8
FlRE-DEATH <robert.bencze@stud.etti.upb.ro>
2024-08-15 11:17:34 +0300

    modified test

--- run_test.sh
+++ run_test.sh
@@ -4,10 +4,11 @@
 #SBATCH --job-name=brTest
 #SBATCH --gres=gpu:1
 
-current_time=$(date +%Y:%m:%d-%H:%M)
+
 #SBATCH --output=slurm-%j-${current_time}.out
 #SBATCH --error=slurm-%j-${current_time}.out
-
+current_time=$(date +%Y:%m:%d-%H:%M)
+output_file="../runLogs/slurm-${SLURM_JOB_ID}-${SLURM_JOB_NAME}-${current_time}.out"
 source ../../venv/bin/activate
 
-python3 hi.py
+python3 hi.py > "$output_file" 2>&1
