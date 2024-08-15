518062f6f68fa29322f8b5204d095dd27c681cee
FlRE-DEATH <robert.bencze@stud.etti.upb.ro>
2024-08-15 10:49:06 +0300

    Testing slurm logs

--- hi.py
+++ hi.py
@@ -1,2 +1,17 @@
+i=0
 while 1:
+    i+=1
     print("hi im a test")
+    if i == 1000:
+        raise Exception("Test error")
+
+
+
+# slurm job stops when closing connection - WHY?
+# job runs slow - WHY?
+
+# multithread the scaler
+# lookup howto reopen console of a job
+# lookup howto allocate more gpus maybe?
+
+# maybe check if scaler looks for max abs per column or if it updates the entire max abs vector
--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-
 #SBATCH --job-name=benczeroAutoMix
-
 #SBATCH --gres=gpu:1
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+
+source ../../venv/bin/activate
 
 source ../../venv/bin/activate
 
