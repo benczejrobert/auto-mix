50ea22b75ecdd1a947aec03a3e502204e16c12d5
FlRE-DEATH <robert.bencze@stud.etti.upb.ro>
2024-08-18 20:04:31 +0300

    Undo last commit

---
+++ hi.py
@@ -0,0 +1,18 @@
+from time import sleep
+i=0
+while 1:
+    i+=1
+    print("hi im a test", i)
+    if i == 1000:
+        raise Exception("Test error")
+
+
+
+# slurm job stops when closing connection - WHY?
+# job runs slow - WHY?
+
+# multithread the scaler
+# lookup howto reopen console of a job
+# lookup howto allocate more gpus maybe?
+
+# maybe check if scaler looks for max abs per column or if it updates the entire max abs vector
